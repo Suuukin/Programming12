85878b5e9c4c6d81d02d447a394f2dbb2578f4cd
Eric Schemenauer <e.c.schemenauer@gmail.com>
2024-04-18 22:37:43 -0700

    Changed labels to buttons, added suggested guess.
    The helper suggests possible guesses based on the frequency of their
    appearances in the possible words. Then based on these frequencies it
    scores all words, and returns the highest scoring.

--- wordle_helper/wordle_helper.py
+++ wordle_helper/wordle_helper.py
@@ -2,10 +2,11 @@ import tkinter as tk
 from tkinter import scrolledtext
 import tkinter.font as font
 import wordlist
+import collections
 
 # This is a modified version of the original template Mr. Poy
 # created. This version is for displaying a list of words
-# that meets the criteria of the wordle you are playing
+# that meets the criteria of the app.state you are playing
 
 window = tk.Tk()
 window.title("Wordle Cheater")
@@ -16,15 +17,22 @@ small_font = font.Font(family="Courier", size=20, weight="bold")
 super_small_font = font.Font(family="Courier", size=20, weight="bold")
 
 
-class Wordle:
+class State:
     def __init__(self):
         self.letter_count = 0
-        self.game_over = False
-        self.word = ""
         self.guess = ""
+        self.green_letters = {}
+        self.yellow_letters = {}
+        self.excluded_words = set()
 
 
-wordle = Wordle()
+class App:
+    def __init__(self):
+        self.word_list = set(wordlist.wordle_list)
+        self.state = State()
+
+
+app = App()
 
 
 def create_label(frame=window):
@@ -32,7 +40,7 @@ def create_label(frame=window):
     new_label = tk.Label(
         frame,
         text=" ",
-        bg="white",
+        bg="grey",
         fg="black",
         font=large_font,
         borderwidth=2,
@@ -46,7 +54,7 @@ def create_button(letter, frame=window):
     new_button = tk.Button(
         frame,
         text=letter,
-        bg="white",
+        bg="grey",
         fg="black",
         font=small_font,
         command=lambda: btn_click(letter),
@@ -55,34 +63,42 @@ def create_button(letter, frame=window):
 
 
 def btn_click(letter):
-    if wordle.letter_count < 5:
-        guess_labels[wordle.letter_count]["text"] = letter
-        wordle.guess += letter
-        wordle.letter_count += 1
+    if app.state.letter_count < 5:
+        guess_buttons[app.state.letter_count]["text"] = letter
+        app.state.guess += letter
+        app.state.letter_count += 1
 
 
 def delete_char():
     # This function will delete the current letter
-    # Use a class variable (wordle.letter_count) to keep track of which label to erase
+    # Use a class variable (app.state.letter_count) to keep track of which label to erase
     # go back a spot as long as you are not in the first spot
-    if wordle.letter_count != 0:
-        wordle.letter_count -= 1
-        wordle.guess = wordle.guess[:-1]
-        guess_labels[wordle.letter_count]["text"] = " "
-        guess_labels[wordle.letter_count]["bg"] = "white"
+    if app.state.letter_count != 0:
+        app.state.letter_count -= 1
+        app.state.guess = app.state.guess[:-1]
+        guess_buttons[app.state.letter_count]["text"] = " "
+        guess_buttons[app.state.letter_count]["bg"] = "grey"
 
 
-def color_label(color):
-    if wordle.letter_count == 0:
-        label_position = wordle.letter_count
-    else:
-        label_position = wordle.letter_count - 1
+def color_label(button_position):
+    # if app.state.letter_count == 0:
+    # label_position = app.state.letter_count
+    # else:
+    # label_position = app.state.letter_count - 1
 
-    guess_labels[label_position]["bg"] = color
+    # guess_buttons[label_position]["bg"] = color
+    button = guess_buttons[button_position]
+    background_color = button["bg"]
+    if background_color == "grey":
+        button["bg"] = "yellow"
+    elif background_color == "yellow":
+        button["bg"] = "green"
+    elif background_color == "green":
+        button["bg"] = "grey"
 
 
 def letter_check(letter_color, letter, word, letter_position, green_letters):
-    if letter_color == "white":
+    if letter_color == "grey":
         if letter not in word or letter in green_letters:
             return True
     elif letter_color == "yellow":
@@ -94,33 +110,66 @@ def letter_check(letter_color, letter, word, letter_position, green_letters):
     return False
 
 
-green_letters = []
+def count_letters(possible_words, green_letters):
+    letter_counter = collections.Counter()
+    for word in possible_words:
+        for c in set(word):
+            letter_counter[c] += 1
+    for letter in green_letters:
+        del letter_counter[letter]
+    print(letter_counter)
+    return letter_counter
+
+
+def evaluate_guesses(letter_counter):
+    score_dict = {}
+
+    def word_score(word):
+        word_score = 0
+        letters = set(word)
+        for c in letters:
+            word_score += letter_counter[c]
+        return word_score
+
+    sorted_words = sorted(app.word_list, reverse=True, key=word_score)
+
+    return sorted_words[:10]
 
 
 def find_possible_words():
     text_area.configure(state="normal")
-
-    word_list = set(wordlist.wordle_list)
-    excluded_words = set()
-    for i, letter in enumerate(wordle.guess):
-        letter_color = guess_labels[i]["bg"]
+    green_letters = []
+    for i, letter in enumerate(app.state.guess):
+        letter_color = guess_buttons[i]["bg"]
         if letter_color == "green":
             if letter not in green_letters:
                 green_letters.append(letter)
-                print(green_letters)
-        for word in (word_list - excluded_words):
+
+        for word in app.word_list - app.state.excluded_words:
             keep_word = letter_check(letter_color, letter, word, i, green_letters)
 
             if not keep_word:
-                excluded_words.add(word)
+                app.state.excluded_words.add(word)
 
+    letter_counter = count_letters(
+        (app.word_list - app.state.excluded_words), green_letters
+    )
+
+    sorted_scores = evaluate_guesses(letter_counter)
+    print(sorted_scores)
     text_area.delete("1.0", tk.END)
-    text_area.insert(tk.END, sorted(word_list - excluded_words))
+    text_area.insert(tk.END, sorted(app.word_list - app.state.excluded_words))
     text_area.configure(state="disabled")
 
 
-# Create 30 labels in a 5 x 6 grid starting at label[0]..label[29]
-# start with an empty dictionary --> label={}  then loop through 30 of them
+def full_reset():
+    app.state = State()
+    for i in guess_buttons:
+        guess_buttons[i].configure(text=" ", bg="grey")
+    text_area.configure(state="normal")
+    text_area.delete("1.0", tk.END)
+    text_area.configure(state="disabled")
+
 
 guess_frame = tk.Frame(window)
 guess_frame.pack(side=tk.TOP)
@@ -131,11 +180,19 @@ keyboard_frame.pack(side=tk.TOP)
 potential_words_frame = tk.Frame(window)
 potential_words_frame.pack(side=tk.TOP)
 
-guess_labels = {}
-for i in range(0, 5):
-    guess_labels[i] = create_label(guess_frame)
+guess_buttons = {}
+#for y in range(0, 6):
+for x in range(0, 5):
+    guess_buttons[x] = button = tk.Button(
+        guess_frame,
+        text=" ",
+        bg="grey",
+        fg="black",
+        font=super_small_font,
+        command=lambda pos=x: color_label(pos),
+    )
     # place the label at an x,y position
-    guess_labels[i].grid(row=0, column=i)
+    button.grid(row=0, column=x)
 
 # Create a keyboard
 # start with an empty dictionary called button = {}, then add elements button[1] etc
@@ -155,7 +212,7 @@ for row, key_row in enumerate(KEY_ROWS):
 del_button = tk.Button(
     keyboard_frames[1],
     text="DEL",
-    bg="white",
+    bg="grey",
     fg="black",
     font=super_small_font,
     command=delete_char,
@@ -166,7 +223,7 @@ window.bind("<BackSpace>", lambda event: delete_char())
 enter_button = tk.Button(
     keyboard_frames[2],
     text="ENTR",
-    bg="white",
+    bg="grey",
     fg="black",
     font=super_small_font,
     command=find_possible_words,
@@ -174,29 +231,19 @@ enter_button = tk.Button(
 enter_button.grid(row=1, column=10)
 window.bind("<Return>", lambda event: find_possible_words())
 
-yellow_button = tk.Button(
-    guess_frame,
-    text=" ",
-    bg="yellow",
-    fg="black",
-    font=super_small_font,
-    command=lambda: color_label("yellow"),
-)
-yellow_button.grid(row=0, column=6)
-
-green_button = tk.Button(
-    guess_frame,
-    text=" ",
-    bg="green",
-    fg="black",
-    font=super_small_font,
-    command=lambda: color_label("green"),
-)
-green_button.grid(row=0, column=7)
-
 text_area = scrolledtext.ScrolledText(
     potential_words_frame, wrap=tk.WORD, width=50, height=30, state="disabled"
 )
 text_area.grid(row=0, column=0, sticky="nsew")
+
+reset_button = tk.Button(
+    potential_words_frame,
+    text="Reset",
+    bg="grey",
+    fg="black",
+    font=super_small_font,
+    command=lambda: full_reset(),
+)
+reset_button.grid(row=1, column=0)
 
 window.mainloop()
