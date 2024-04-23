30b55afc93eb8740ca3307af21764f896af76335
Suuukin <115589454+Suuukin@users.noreply.github.com>
2024-04-23 14:55:31 -0700

    Added simple comments and slight formtting change

--- wordle_helper/wordle_helper.py
+++ wordle_helper/wordle_helper.py
@@ -14,10 +14,10 @@ window.resizable(False, False)
 
 large_font = font.Font(family="Courier", size=40, weight="bold")
 small_font = font.Font(family="Courier", size=20, weight="bold")
-super_small_font = font.Font(family="Courier", size=20, weight="bold")
 
 
 class Char:
+    # Class tracks the qualities of the guess buttons.
     def __init__(self, row, column):
         self.row = row
         self.column = column
@@ -26,13 +26,14 @@ class Char:
 
 
 class Row:
+    # Class representing each guess row.
     def __init__(self, row_number):
         self.row = row_number
         self.guess = ""
-        self.result = None
         self.letter_count = 0
 
     def delete_char(self, char_dict):
+        # Removes the last letter in current row
         if self.letter_count != 0:
             x = self.letter_count - 1
             y = self.row
@@ -44,6 +45,7 @@ class Row:
             guess_buttons[(x, y)]["bg"] = "grey"
 
     def add_char(self, letter, char_dict):
+        # Adds character to the button with position (x, y)
         if self.letter_count < 5:
             (x, y) = (self.letter_count, self.row)
             char_dict[(x, y)].character = letter
@@ -53,6 +55,8 @@ class Row:
 
 
 class State:
+    """Class holds all variables and objects that need to be reset."""
+
     def __init__(self):
         self.current_row = 0
         self.guess_rows = {}
@@ -73,6 +77,8 @@ class State:
 
 
 class App:
+    """Main class, holds things that persist."""
+
     def __init__(self):
         self.word_list = set(wordlist.wordle_list)
         self.state = State()
@@ -100,6 +106,7 @@ def create_button(letter, frame=window):
 
 
 def color_label(button_position):
+    # changes the color of the clicked button
     button = guess_buttons[button_position]
     background_color = app.state.char_dict[button_position].color
     if background_color == "grey":
@@ -114,6 +121,10 @@ def color_label(button_position):
 
 
 def letter_check(letter_color, letter, word, letter_position, green_letters):
+    """
+    returns if the input word is a possible guess,
+    based off the positions of it's letters and colors.
+    """
     if letter_color == "grey":
         if letter not in word or letter in green_letters:
             return True
@@ -127,6 +138,11 @@ def letter_check(letter_color, letter, word, letter_position, green_letters):
 
 
 def count_letters(possible_words, green_letters):
+    """
+    Returns a Counter object that has the frequency of all letters
+    in the remaining possible guesses. Only counts the first
+    appearance of each letter in a word.
+    """
     letter_counter = collections.Counter()
     for word in possible_words:
         for c in set(word):
@@ -137,6 +153,13 @@ def count_letters(possible_words, green_letters):
 
 
 def evaluate_guesses(letter_count, word_list):
+    """
+    Returns a list of input words sorted in descending order
+    based on their score.
+    Scores words based on the frequency of the letters they contain.
+    Gives no points for green letters, and gives no points for words
+    with letters that are known to be yellow in their position.
+    """
 
     def word_score(word):
         word_score = 0
@@ -158,6 +181,11 @@ def evaluate_guesses(letter_count, word_list):
 
 
 def find_possible_words():
+    """
+    Takes all input guesses and colors then
+    fills text_area with possible guesses, suggested guesses from all words,
+    and suggested guesses from remaining words.
+    """
     text_area.configure(state="normal")
     green_letters = []
     excluded_words = set()
@@ -181,7 +209,11 @@ def find_possible_words():
     possible_guesses = evaluate_guesses(
         letter_counter, (app.word_list - excluded_words)
     )
-    guess_string = f"Suggested Guesses: {suggested_guesses} \n\nGuesses from Possible Words: {possible_guesses} \n\n"
+
+    suggested_string = ", ".join(suggested_guesses)
+    possible_string = ", ".join(possible_guesses)
+
+    guess_string = f"Suggested Guesses: {suggested_string} \n\nGuesses from Possible Words: {possible_string} \n\n"
 
     text_area.delete("1.0", tk.END)
     text_area.insert(tk.END, guess_string)
@@ -192,6 +224,9 @@ def find_possible_words():
 
 
 def full_reset():
+    """
+    Makes a new copy of State() and clears text area and guess buttons.
+    """
     app.state = State()
     for i in guess_buttons:
         guess_buttons[i].configure(text=" ", bg="grey")
@@ -200,19 +235,17 @@ def full_reset():
     text_area.configure(state="disabled")
 
 
-main_frame = tk.Frame(window)
-main_frame.grid(row=0, column=0)
-
-guess_frame = tk.Frame(main_frame)
+guess_frame = tk.Frame(window)
 guess_frame.pack(side=tk.TOP)
 
-keyboard_frame = tk.Frame(main_frame)
+keyboard_frame = tk.Frame(window)
 keyboard_frame.pack(side=tk.TOP)
 
-potential_words_frame = tk.Frame(main_frame)
+potential_words_frame = tk.Frame(window)
 potential_words_frame.pack(side=tk.TOP)
 
 guess_buttons = {}
+# creates a 5x6 grid of buttons that act as labels.
 for y in range(6):
     for x in range(5):
         guess_buttons[(x, y)] = button = tk.Button(
@@ -220,14 +253,14 @@ for y in range(6):
             text=" ",
             bg="grey",
             fg="black",
-            font=super_small_font,
+            font=small_font,
             command=lambda pos=(x, y): color_label(pos),
         )
         # place the label at an x,y position
         button.grid(row=y, column=x)
 
-# Create a keyboard
-# start with an empty dictionary called button = {}, then add elements button[1] etc
+# Creates a keyboard based on the rows of keys on QWERTY keyboard.
+# Stores the created buttons in a dictionary based on their string.
 KEY_ROWS = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
 keyboard_frames = {}
 for row, key_row in enumerate(KEY_ROWS):
@@ -246,7 +279,7 @@ del_button = tk.Button(
     text="DEL",
     bg="grey",
     fg="black",
-    font=super_small_font,
+    font=small_font,
     command=app.do_delete_char,
 )
 del_button.grid(row=1, column=12)
@@ -260,7 +293,7 @@ enter_button = tk.Button(
     text="ENTR",
     bg="grey",
     fg="black",
-    font=super_small_font,
+    font=small_font,
     command=find_possible_words,
 )
 enter_button.grid(row=1, column=10)
@@ -276,7 +309,7 @@ reset_button = tk.Button(
     text="Reset",
     bg="grey",
     fg="black",
-    font=super_small_font,
+    font=small_font,
     command=lambda: full_reset(),
 )
 reset_button.grid(row=1, column=0)
